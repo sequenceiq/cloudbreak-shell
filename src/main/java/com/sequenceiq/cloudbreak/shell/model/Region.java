2f8b51e38827408602d05052b723c62726856a25
doktoric <doktoricsrichard@gmail.com>
2014-08-14 10:20:05 +0200

    CLOUD-186 fixed amis

--- src/main/java/com/sequenceiq/cloudbreak/shell/model/Region.java
+++ src/main/java/com/sequenceiq/cloudbreak/shell/model/Region.java
@@ -2,14 +2,14 @@ package com.sequenceiq.cloudbreak.shell.model;
 
 public enum Region {
 
-    US_EAST_1("ami-d89955b0"),
-    US_WEST_1("ami-314a4974"),
-    US_WEST_2("ami-17b0c927"),
-    EU_WEST_1("ami-7778af00"),
-    AP_SOUTHEAST_1("ami-468bd214"),
-    AP_SOUTHEAST_2("ami-678bec5d"),
-    AP_NORTHEAST_1("ami-0bcd9d0a"),
-    SA_EAST_1("ami-1b0ca206");
+    US_EAST_1("ami-0609d86e"),
+    US_WEST_1("ami-73000d36"),
+    US_WEST_2("ami-07bdc737"),
+    EU_WEST_1("ami-e09e4397"),
+    AP_SOUTHEAST_1("ami-363f6464"),
+    AP_SOUTHEAST_2("ami-3d9ef807"),
+    AP_NORTHEAST_1("ami-8dbde48c"),
+    SA_EAST_1("ami-17f45c0a");
 
     private String ami;
 
