cf2baf3b062398dc2fad2f3c726ac4c04e779850
Tamas Bihari <tamas.bihari@sequenceiq.com>
2014-10-16 16:50:09 +0200

    CLOUD-216 update AMI ids

--- src/main/java/com/sequenceiq/cloudbreak/shell/model/Region.java
+++ src/main/java/com/sequenceiq/cloudbreak/shell/model/Region.java
@@ -2,14 +2,14 @@ package com.sequenceiq.cloudbreak.shell.model;
 
 public enum Region {
 
-    US_EAST_1("ami-0609d86e"),
-    US_WEST_1("ami-73000d36"),
-    US_WEST_2("ami-07bdc737"),
-    EU_WEST_1("ami-e09e4397"),
-    AP_SOUTHEAST_1("ami-363f6464"),
-    AP_SOUTHEAST_2("ami-3d9ef807"),
-    AP_NORTHEAST_1("ami-8dbde48c"),
-    SA_EAST_1("ami-17f45c0a");
+    US_EAST_1("ami-5edf6136"),
+    US_WEST_1("ami-4fc1cb0a"),
+    US_WEST_2("ami-29367a19"),
+    EU_WEST_1("ami-e66cc191"),
+    AP_SOUTHEAST_1("ami-7cedcb2e"),
+    AP_SOUTHEAST_2("ami-2f385515"),
+    AP_NORTHEAST_1("ami-757e4974"),
+    SA_EAST_1("ami-91f0448c");
 
     private String ami;
 
