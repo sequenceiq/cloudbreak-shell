459f824fc1e2075a723a1b129dfd2be4f2376c33
Krisztian Horvath <krisztian.horvath@sequenceiq.com>
2015-10-13 09:28:31 +0200

    CLOUD-45976 Azure D_V2 instance types

--- src/main/java/com/sequenceiq/cloudbreak/shell/model/AzureInstanceType.java
+++ src/main/java/com/sequenceiq/cloudbreak/shell/model/AzureInstanceType.java
@@ -6,16 +6,15 @@ public enum AzureInstanceType {
     A7,
     A8,
     A9,
-    BASIC_A0,
-    BASIC_A1,
-    BASIC_A2,
-    BASIC_A3,
-    BASIC_A4,
+    A10,
+    A11,
+
     STANDARD_G1,
     STANDARD_G2,
     STANDARD_G3,
     STANDARD_G4,
     STANDARD_G5,
+
     STANDARD_D1,
     STANDARD_D2,
     STANDARD_D3,
@@ -23,5 +22,15 @@ public enum AzureInstanceType {
     STANDARD_D11,
     STANDARD_D12,
     STANDARD_D13,
-    STANDARD_D14
+    STANDARD_D14,
+
+    STANDARD_D1_V2,
+    STANDARD_D2_V2,
+    STANDARD_D3_V2,
+    STANDARD_D4_V2,
+    STANDARD_D5_V2,
+    STANDARD_D11_V2,
+    STANDARD_D12_V2,
+    STANDARD_D13_V2,
+    STANDARD_D14_V2;
 }
